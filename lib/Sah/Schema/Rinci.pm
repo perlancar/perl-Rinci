2833d3bf9592c75642f48f211d147002a5228b00
Steven Haryanto (on Asus Zenbook) <stevenharyanto@gmail.com>
2014-04-26 13:49:12 +0700

    Add Sah::Schema::Rinci

---
+++ lib/Sah/Schema/Rinci.pm
@@ -0,0 +1,142 @@
+package Sah::Schema::Rinci;
+
+use 5.010001;
+use strict;
+use warnings;
+
+# VERSION
+
+our %SCHEMAS;
+
+$SCHEMAS{rinci} = [hash => {
+    # tmp
+    _prop => {
+        # from defhash
+        v => {},
+        defhash_v => {},
+        name => {},
+        summary => {},
+        description => {},
+        tags => {},
+        default_lang => {},
+        x => {},
+
+        entity_v => {},
+        links => {
+            _elem_prop => {
+                # XXX include from defhash
+
+                uri => {},
+                title => {},
+            },
+        },
+    },
+}];
+
+$SCHEMAS{rinci_function} = [hash => {
+    # tmp
+    _prop => {
+        # from defhash
+        v => {},
+        defhash_v => {},
+        name => {},
+        summary => {},
+        description => {},
+        tags => {},
+        default_lang => {},
+        x => {},
+
+        # from common rinci metadata
+        entity_v => {},
+        links => {},
+
+        is_func => {},
+        is_meth => {},
+        is_class_meth => {},
+        args => {
+            _elem_prop => {
+                # from defhash
+                summary => {},
+                description => {},
+                tags => {},
+
+                schema => {},
+                default => {},
+                req => {},
+                pos => {},
+                greedy => {},
+                cmdline_aliases => {},
+                cmdline_on_getopt => {},
+                completion => {},
+                element_completion => {},
+                cmdline_src => {},
+            },
+        },
+        args_as => {},
+        result => {
+            _prop => {
+                # from defhash
+                summary => {},
+                description => {},
+
+                schema => {},
+                statuses => {
+                    _val_prop => {
+                        # from defhash
+                        summary => {},
+                        description => {},
+
+                        schema => {},
+                    },
+                },
+            },
+        },
+        result_naked => {},
+        examples => {
+            _elem_prop => {
+                # from defhash
+                summary => {},
+                description => {},
+                tags => {},
+
+                args => {},
+                argv => {},
+                src => {},
+                src_plang => {},
+                status => {},
+                result => {},
+                test => {},
+            },
+        },
+        features => {
+            _prop => {
+                reverse => {},
+                tx => {},
+                dry_run => {},
+                pure => {},
+                immutable => {},
+                idempotent => {},
+            },
+        },
+        deps => {
+            _prop => {
+                all => {},
+                any => {},
+                none => {},
+                env => {},
+                prog => {},
+                code => {},
+                tmp_dir => {},
+                trash_dir => {},
+            },
+        },
+    },
+}];
+
+# rinci_package
+# rinci_variable
+# rinci_result
+
+
+1;
+# ABSTRACT: Sah schemas for Rinci metadata
